bbe844d58c6eecfad4049b149240f4921390dcb9
Rajat Tiwari <rajattiwari1998r@gmail.com>
2025-01-02 12:48:46 +0530

    Create maximun minimum value that int and float contains.C

---
+++ maximun minimum value that int and float contains.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    int max= INT_MAX;
+    int min = INT_MIN;
+    
+    long max1 = LONG_MAX;
+    long min1 = LONG_MIN;
+     
+    
+    printf("%d\n",max);
+    printf("%d\n",min);
+    
+     printf("%ld\n",max1);
+     printf("%ld\n",min1);
+    
+    return 0;
+}
